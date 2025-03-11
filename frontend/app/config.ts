3be9ca886668b92f31f13474533f3ac21b9e3602
Zaid <zaidkh1303@gmail.com>
2025-03-11 15:39:10 +0530

    change backend and hook url

--- frontend/app/config.ts
+++ frontend/app/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL="http://localhost:3000";
-export const HOOKS_URL="http://ec2-3-84-86-117.compute-1.amazonaws.com:3002";
+export const BACKEND_URL="http://ec2-23-22-232-149.compute-1.amazonaws.com:3000";
+export const HOOKS_URL="http://ec2-54-227-226-242.compute-1.amazonaws.com:3002";
